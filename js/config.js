673a988fe3b44f3d41fe1d98ff5652d58eac6f85
Ben Krocké <benkrocke@hotmail.com>
2020-05-27 10:35:20 +0200

    Initialized Repository

---
+++ js/config.js
@@ -0,0 +1,152 @@
+var respecConfig = {
+    specStatus: "DK-WV",
+    specType: "HR",
+    subtitle: "Digikoppeling Koppelvlakstandaard ebMS2",
+    license: "cc-by",
+    edDraftURI: "https://gitlab.com/logius/oidc/-/blob/master/NLGov-OpenIDConnect-profile.md",
+    otherLinks: [{
+        key: "Participate",
+        data: [{
+        value: "Github",
+            href: "https://github.com/centrumvoorstandaarden/OIDC"
+        }, {
+        value: "File a bug",
+            href: "https://github.com/centrumvoorstandaarden/OIDC/issues"
+        }, {
+        value: "Commit history",
+            href: "https://github.com/centrumvoorstandaarden/OIDC/commits/master"
+        }, {
+        value: "Merge requests",
+            href: "https://github.com/centrumvoorstandaarden/OIDC/pulls"
+        }]
+    }],
+    editors: [{
+        name: "Remco Schaar",
+        company: "Logius",
+        url: "https://www.logius.nl",
+    },{
+        name: "Frank van Es",
+        company: "Logius",
+        url: "https://www.logius.nl",
+    },{
+        name: "Pieter Hering",
+        company: "Logius/Centrum voor Standaarden",
+        url: "https://www.logius.nl",
+    }],
+    authors: [{
+        name: "Remco Schaar",
+        company: "Logius",
+        url: "https://www.logius.nl",
+    },{
+        name: "Frank van Es",
+        company: "Logius",
+        url: "https://www.logius.nl",
+    },{
+        name: "Joris Joosten",
+        company: "VZVZ",
+        url: "https://www.vzvz.nl",
+    },{
+        name: "Jan Geert Koops",
+        company: "Dictu",
+        url: "https://www.dictu.nl",
+    }],
+    mdn: true,
+    localBiblio: {
+            "ebMS3": {
+                href: "https://docs.oasis-open.org/ebxml-msg/ebms/v3.0/core/ebms_core-3.0-spec.pdf",
+                title:
+                "OASIS ebXML Messaging Services 3 Version 3.0: Part 1, Core Features",
+                authors: ["Ian Jones", "Pete Wenzel"],
+                date: "October 2007",
+                publisher: "OASIS",
+            },
+            "BPSS": {
+                href: "http://www.ebxml.org/specs/ebBPSS.pdf",
+                title:
+                "ebXML Business Process Specification Schema",
+                authors: ["Paul  Levine"],
+                date: "May 2001",
+                publisher: "OASIS",
+            },
+            "LO.GBA": {
+                href: "https://www.rvig.nl/documenten/publicaties/2019/06/26/logisch-ontwerp-gba-versie-3.12",
+                title: " Logisch ontwerp GBA",
+                authors: [""],
+                date: "26 juni 2019",
+                publisher: "RvIG",
+            },
+            "OAuth2.1": {
+                href: "https://tools.ietf.org/html/draft-parecki-oauth-v2-1",
+                title: "OAuth 2.1 Working draft",
+                authors: ["D. Hardt", "A. Parecki", "T. Lodderstedt"],
+                date: "April 2020",
+                publisher: "IETF OAuth Working Group"
+            },
+            "OAuth2.NLGov": {
+                href: "https://docs.geostandaarden.nl/api/oauth/",
+                title:
+                "NL GOV Assurance profile for OAuth 2.0",
+                authors: ["Frank Terpstra", "Jan van Gelder"],
+                date: "2019",
+                publisher: "Geonovum",
+            },
+            "OpenID.Core": {
+                href: "https://openid.net/specs/openid-connect-core-1_0.html",
+                title: "OpenID Connect Core 1.0",
+                authors: [
+                "Nat Sakimura", "John Bradley", "Michael B. Jones", 
+                "Breno de Medeiros", "Chuck Mortimore"],
+                date: "2014",
+                publisher: "The OpenID Foundation",
+            },
+            "OpenID.Discovery": {
+                href: "https://openid.net/specs/openid-connect-discovery-1_0.html",
+                title: "OpenID Connect Discovery 1.0",
+                authors: [
+                "N. Sakimura", "J. Bradley", "M. Jones", 
+                "E. Jay"],
+                date: "2014",
+                publisher: "The OpenID Foundation",
+            },
+            "OpenID.iGov": {
+                href: "https://openid.net/specs/openid-igov-openid-connect-1_0.html",
+                title: "International Government Assurance Profile (iGov) for OpenID Connect 1.0",
+                authors: [
+                "Michael Varley", "Paul Grassi"],
+                date: "2018",
+                publisher: "The OpenID Foundation",
+            },
+            "PKI-CA": {
+                href: "https://www.logius.nl/diensten/pkioverheid/aansluiten-als-tsp/toegetreden-vertrouwensdienstverleners",
+                title:
+                "Toegetreden vertrouwensdienstverleners",
+                authors: [""],
+                date: "2018",
+                publisher: "Logius",
+            },
+            "PKI.Policy": {
+                href: "https://www.logius.nl/sites/default/files/public/bestanden/diensten/PKIoverheid/Deel2-eisen-voor-TSPs-v4.7.pdf",
+                title:
+                "Programma van Eisen deel 2: Toetreding tot en toezicht binnen de PKI voor de overheid",
+                authors: [""],
+                date: "8 februari 2019",
+                publisher: "Logius",
+            },
+            "SG.TLS": {
+                href: "https://english.ncsc.nl/publications/publications/2019/juni/01/it-security-guidelines-for-transport-layer-security-tls",
+                title: " IT Security Guidelines for Transport Layer Security (TLS)",
+                authors: [""],
+                date: "23-05-2019",
+                publisher: "NCSC",
+            },
+            "UMMR10": {
+                href: "https://www.unece.org/fileadmin/DAM/cefact/umm/UMM_Revision_10_2001.zip",
+                title:
+                "UN/CEFACT Modelling Methodology (UMM) Revison 10",
+                authors: [""],
+                date: "2001",
+                publisher: "UN/CEFACT",
+            },
+
+            },
+};
