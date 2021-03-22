b23004a7e9fc5e61fcc31fdbccab74b582ef9cf0
PieterHering <48095354+PieterHering@users.noreply.github.com>
2021-03-22 16:17:18 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -38,8 +38,8 @@ var respecConfig = {
     // A YYYY-MM-DD date. When there is a previous release of a given specification, (W3C)
     previousPublishDate: "2020-09-01",
 
-    publishVersion:"2.11",
-    previousPublishVersion:"2.10",
+    publishVersion:"2.12",
+    previousPublishVersion:"2.11",
   
     // Zie https://github.com/w3c/respec/wiki/previousMaturity. Dit moet een
     // A YYYY-MM-DD date. When a previousPublishDate is specified, this is typically required as well in order to generate the "Previous Version"
